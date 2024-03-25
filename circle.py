98a4d8ab1ca9be504866a5453c1b9f432d7bae78
souilemaymen <109996290+souilemaymen@users.noreply.github.com>
2024-03-25 01:18:29 +0100

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,17 @@
+from turtle import * 
+bgcolor('cyan')
+speed(0)
+hideturtle()
+for i in range(120):
+    color('blue')
+    circle(i)
+    color('red')
+    circle(i*0.8)
+    right(3)
+    forward(3)
+done()
+
+
+
+
+
